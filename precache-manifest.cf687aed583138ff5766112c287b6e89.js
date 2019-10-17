2901dff32aa2a92c37012cfe5cbf7afd194ff025
Truong Nguyen <samuraitruong@hotmail.com>
2019-10-17 22:43:41 +1100

    Updates

---
+++ precache-manifest.cf687aed583138ff5766112c287b6e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a789d262b62bb738190154e45215d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b845479280755c3126bd",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b6452a0dbfcef664fcb4",
+    "url": "/static/js/2.3c9887e1.chunk.js"
+  },
+  {
+    "revision": "b845479280755c3126bd",
+    "url": "/static/js/main.d6ccb2d3.chunk.js"
+  },
+  {
+    "revision": "ab2a8ac59a4d4155cd97",
+    "url": "/static/js/runtime-main.b4a39de6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
